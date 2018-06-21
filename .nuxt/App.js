e8b1e197a9fc8f5d90f0f2e78ecd4e15a966678f
heliang <heliang@duyao001.com>
2018-06-21 10:40:35 +0800

    提交

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -23,7 +23,7 @@ let layouts = {
 let resolvedLayouts = {}
 
 export default {
-  head: {"title":"首页","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"首页"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+  head: {"title":"前端日记","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"首页"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
   render(h, props) {
     const loadingEl = h('nuxt-loading', { ref: 'loading' })
     const layoutEl = h(this.layout || 'nuxt')
